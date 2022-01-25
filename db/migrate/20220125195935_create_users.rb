79cef8fd2b7fff499d792df7afe3ae5561ca1f5a
David Moina <davemolina777@gmail.com>
2022-01-25 14:03:08 -0600

    Add validations and user attributes

--- db/migrate/20220125195935_create_users.rb
+++ db/migrate/20220125195935_create_users.rb
@@ -1,10 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :firstName
+      t.string :lastName
       t.string :email
-      t.string :nickname
-
+      t.string :username
+      t.string :password_digest
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_25_171023) do
+ActiveRecord::Schema.define(version: 2022_01_25_195935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,16 @@ ActiveRecord::Schema.define(version: 2022_01_25_171023) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "rating"
     t.string "format"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "firstName"
+    t.string "lastName"
+    t.string "email"
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
