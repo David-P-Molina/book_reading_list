926536bfbc0862d1a761a9e000d8da0214709083
David Moina <davemolina777@gmail.com>
2022-01-27 14:30:22 -0600

    Create basic validations

--- app/models/user_book.rb
+++ app/models/user_book.rb
@@ -1,4 +1,8 @@
 class UserBook < ApplicationRecord
+    validates :user_id, :start_date, presence: true
+    validates :percent_read, numercality: true
+    #custom validation for start and end date
+    
     belongs_to :book
-    # belongs_to :user
+    belongs_to :user
 end
