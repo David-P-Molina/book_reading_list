e56c1b1c3522b00822ef2181499181638912de39
David Moina <davemolina777@gmail.com>
2022-01-27 14:27:47 -0600

    Add custom validators and basic validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,9 @@
 class Book < ApplicationRecord
+    validates :title, presence: true
+    validates :page_count, numercality: true
+    # custom validation for  publish date, series boolean and if boolean true series_number
+    #custom validation for isbn to include x but mainly have numbers
+
     belongs_to :author
     belongs_to :genre
 end
