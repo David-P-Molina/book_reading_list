dd97cf4354499fc75f7bd19a827f60b4039e0807
David Moina <davemolina777@gmail.com>
2022-01-27 14:22:11 -0600

    Create basic validations and plan custom validations

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,9 @@
 class Author < ApplicationRecord
+    validates :name, :biography, presence: true
+    validates :biography, length: { minimum: 20 }
+    #custom validator for birth/death dates format
+    #custom validator for twitter and website format
+    
     has_many :books
     has_many :genres, through: :books
 end
